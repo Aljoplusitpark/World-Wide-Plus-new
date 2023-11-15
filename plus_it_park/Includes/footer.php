5a92fc23d84047e91e3ce7c47d8201acf00c9a25
AljoAntony24 <aljoantony24@gmail.com>
2023-11-15 17:34:46 +0530

    indexfile

---
+++ plus_it_park/Includes/footer.php
@@ -0,0 +1,243 @@
+<footer class="cs-fooer">
+      <div class="cs-fooer_main">
+        <div class="container">
+          <div class="row">
+            <div class="col-lg-3 col-sm-6">
+              <div class="cs-footer_item">
+                <div class="cs-text_widget">
+                  <img src="assets/img/logoo.png" alt="Thumb">
+                  <p style="text-align:justify;"> A space to celebrate your life, technology, technocracy, friendship, passion and more. We promote security and confidence for your imaginations by creating differentiated Insights  </p>
+                </div>
+                <div class="cs-social_btns cs-style1">
+                  <a href="#" class="cs-center" target="-blank">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-whatsapp" viewBox="0 0 16 16"> <path d="M13.601 2.326A7.854 7.854 0 0 0 7.994 0C3.627 0 .068 3.558.064 7.926c0 1.399.366 2.76 1.057 3.965L0 16l4.204-1.102a7.933 7.933 0 0 0 3.79.965h.004c4.368 0 7.926-3.558 7.93-7.93A7.898 7.898 0 0 0 13.6 2.326zM7.994 14.521a6.573 6.573 0 0 1-3.356-.92l-.24-.144-2.494.654.666-2.433-.156-.251a6.56 6.56 0 0 1-1.007-3.505c0-3.626 2.957-6.584 6.591-6.584a6.56 6.56 0 0 1 4.66 1.931 6.557 6.557 0 0 1 1.928 4.66c-.004 3.639-2.961 6.592-6.592 6.592zm3.615-4.934c-.197-.099-1.17-.578-1.353-.646-.182-.065-.315-.099-.445.099-.133.197-.513.646-.627.775-.114.133-.232.148-.43.05-.197-.1-.836-.308-1.592-.985-.59-.525-.985-1.175-1.103-1.372-.114-.198-.011-.304.088-.403.087-.088.197-.232.296-.346.1-.114.133-.198.198-.33.065-.134.034-.248-.015-.347-.05-.099-.445-1.076-.612-1.47-.16-.389-.323-.335-.445-.34-.114-.007-.247-.007-.38-.007a.729.729 0 0 0-.529.247c-.182.198-.691.677-.691 1.654 0 .977.71 1.916.81 2.049.098.133 1.394 2.132 3.383 2.992.47.205.84.326 1.129.418.475.152.904.129 1.246.08.38-.058 1.171-.48 1.338-.943.164-.464.164-.86.114-.943-.049-.084-.182-.133-.38-.232z" fill="white"/> </svg>
+                    
+                    
+                  </a>
+                  <a href="https://www.facebook.com/profile.php?id=100094438314339" class="cs-center" target="-blank">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-facebook" viewBox="0 0 16 16"> <path d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951z" fill="white"/> </svg>
+                  </a>
+                  <a href="https://www.linkedin.com/company/plusitpark" class="cs-center" target="-blank">
+                    <svg width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg">
+                      <path d="M4.04799 13.7497H1.45647V5.4043H4.04799V13.7497ZM2.75084 4.2659C1.92215 4.2659 1.25 3.57952 1.25 2.75084C1.25 2.35279 1.40812 1.97105 1.68958 1.68958C1.97105 1.40812 2.35279 1.25 2.75084 1.25C3.14888 1.25 3.53063 1.40812 3.81209 1.68958C4.09355 1.97105 4.25167 2.35279 4.25167 2.75084C4.25167 3.57952 3.57924 4.2659 2.75084 4.2659ZM13.7472 13.7497H11.1613V9.68722C11.1613 8.71903 11.1417 7.4774 9.81389 7.4774C8.46652 7.4774 8.26004 8.5293 8.26004 9.61747V13.7497H5.67132V5.4043H8.15681V6.54269H8.19308C8.53906 5.887 9.38421 5.19503 10.6451 5.19503C13.2679 5.19503 13.75 6.92215 13.75 9.16546V13.7497H13.7472Z" fill="white" />
+                    </svg>
+                    
+                  </a>
+                  
+                  <a href="https://www.instagram.com/plusitpark/" class="cs-center" target="-blank">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-instagram" viewBox="0 0 16 16"> <path d="M8 0C5.829 0 5.556.01 4.703.048 3.85.088 3.269.222 2.76.42a3.917 3.917 0 0 0-1.417.923A3.927 3.927 0 0 0 .42 2.76C.222 3.268.087 3.85.048 4.7.01 5.555 0 5.827 0 8.001c0 2.172.01 2.444.048 3.297.04.852.174 1.433.372 1.942.205.526.478.972.923 1.417.444.445.89.719 1.416.923.51.198 1.09.333 1.942.372C5.555 15.99 5.827 16 8 16s2.444-.01 3.298-.048c.851-.04 1.434-.174 1.943-.372a3.916 3.916 0 0 0 1.416-.923c.445-.445.718-.891.923-1.417.197-.509.332-1.09.372-1.942C15.99 10.445 16 10.173 16 8s-.01-2.445-.048-3.299c-.04-.851-.175-1.433-.372-1.941a3.926 3.926 0 0 0-.923-1.417A3.911 3.911 0 0 0 13.24.42c-.51-.198-1.092-.333-1.943-.372C10.443.01 10.172 0 7.998 0h.003zm-.717 1.442h.718c2.136 0 2.389.007 3.232.046.78.035 1.204.166 1.486.275.373.145.64.319.92.599.28.28.453.546.598.92.11.281.24.705.275 1.485.039.843.047 1.096.047 3.231s-.008 2.389-.047 3.232c-.035.78-.166 1.203-.275 1.485a2.47 2.47 0 0 1-.599.919c-.28.28-.546.453-.92.598-.28.11-.704.24-1.485.276-.843.038-1.096.047-3.232.047s-2.39-.009-3.233-.047c-.78-.036-1.203-.166-1.485-.276a2.478 2.478 0 0 1-.92-.598 2.48 2.48 0 0 1-.6-.92c-.109-.281-.24-.705-.275-1.485-.038-.843-.046-1.096-.046-3.233 0-2.136.008-2.388.046-3.231.036-.78.166-1.204.276-1.486.145-.373.319-.64.599-.92.28-.28.546-.453.92-.598.282-.11.705-.24 1.485-.276.738-.034 1.024-.044 2.515-.045v.002zm4.988 1.328a.96.96 0 1 0 0 1.92.96.96 0 0 0 0-1.92zm-4.27 1.122a4.109 4.109 0 1 0 0 8.217 4.109 4.109 0 0 0 0-8.217zm0 1.441a2.667 2.667 0 1 1 0 5.334 2.667 2.667 0 0 1 0-5.334z"  fill="white"/> </svg>
+                  </a>
+                  <a href="#" class="cs-center" target="-blank">
+                    <svg width="13" height="9" viewBox="0 0 13 9" fill="none" xmlns="http://www.w3.org/2000/svg" >
+                      <path d="M12.4888 1.48066C12.345 0.939353 11.9215 0.513038 11.3837 0.368362C10.4089 0.105469 6.5 0.105469 6.5 0.105469C6.5 0.105469 2.59116 0.105469 1.61633 0.368362C1.07853 0.513061 0.65496 0.939353 0.5112 1.48066C0.25 2.4618 0.25 4.50887 0.25 4.50887C0.25 4.50887 0.25 6.55595 0.5112 7.53709C0.65496 8.0784 1.07853 8.48695 1.61633 8.63163C2.59116 8.89452 6.5 8.89452 6.5 8.89452C6.5 8.89452 10.4088 8.89452 11.3837 8.63163C11.9215 8.48695 12.345 8.0784 12.4888 7.53709C12.75 6.55595 12.75 4.50887 12.75 4.50887C12.75 4.50887 12.75 2.4618 12.4888 1.48066V1.48066ZM5.22158 6.36746V2.65029L8.48861 4.50892L5.22158 6.36746V6.36746Z" fill="white" />
+                    </svg>
+                  </a>
+                  
+                </div>
+                
+              </div>
+                <div class="cs-footer_item">
+                  <h2 class="cs-widget_title" style="color:#cccc00;">Subscribe Now!</h2>
+                  <div class="cs-newsletter cs-style1">
+                    <!-- <div class="cs-newsletter_text">Get In Touch With Us</div> -->
+                    <form action="#" class="cs-newsletter_form">
+                      <input type="email" class="cs-newsletter_input" placeholder="example@gmail.com">
+                      <button class="cs-newsletter_btn" style="background-color:#cccc00;">
+                        <span >Send</span>
+                      </button>
+                    </form>
+                  </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-sm-6">
+              <div class="cs-footer_item">
+                
+                <h2 class="cs-widget_title" style="color:#cccc00;">Designing</h2>
+                <ul class="cs-menu_widget cs-mp0">
+                  <li><a href="#">UI / UX Designing</a></li>
+                  <li><a href="#">Search Engine Optimization</a></li>
+                  <li><a href="#">Responsive Web Designing</a></li>
+                  <li><a href="#">Corporate Websites</a></li>
+                  <li><a href="#">Interactive Web Designing</a></li>
+                  <li><a href="#">Customized Websites</a></li>
+                  <!-- <li><a href="#">Mobile App Development</a></li> -->
+                </ul>
+                <br><br><br>
+                <h2 class="cs-widget_title" style="color:#cccc00;">Quick Links</h2>
+                <ul class="cs-menu_widget cs-mp0">
+                  <li>
+                    <a href="#">Home</a>
+                  </li>
+                  <li>
+                    <a href="#">What We Do</a>
+                  </li>
+                  <li>
+                    <a href="#">About Us</a>
+                  </li>
+                  <li>
+                    <a href="#">Organizations</a>
+                  </li>
+                  <li>
+                    <a href="#">Life at Plus IT Park</a>
+                  </li>
+                  <!-- <li>
+                    <a href="#">DSF</a>
+                  </li> -->
+                </ul>
+              </div>
+            </div>
+            <div class="col-lg-2 col-sm-6">
+              <div class="cs-footer_item">
+                <h2 class="cs-widget_title" style="color:#cccc00;">Development</h2>
+                <ul class="cs-menu_widget cs-mp0">
+                <li><a href="#">Web3 Development</a></li>
+                  <li><a href="#">Web Development</a></li>
+                  <li><a href="#">E-Commerce</a></li>
+                  <li><a href="#">Content Management</a></li>
+                  <li><a href="#">B2B Platform</a></li>
+                  <li><a href="#">ERP / CRM</a></li>
+                  <li><a href="#">Backend Solution</a></li>
+                  <li><a href="#">Mobile App Development</a></li>
+                </ul>
+                <br>
+                <h2 class="cs-widget_title" style="color:#cccc00;">Graphic Designing</h2>
+                <ul class="cs-menu_widget cs-mp0">
+                  <li><a href="#">Logo Designing</a></li>
+                  <li><a href="#">Branding</a></li>
+                  <li><a href="#">2D & 3D Annimation</a></li>
+                  <li><a href="#">Business Presentation</a></li>
+                  <li><a href="#">White Board Animation</a></li>
+                  <!-- <li><a href="#">B2B Listening</a></li> -->
+                  <!-- <li><a href="#">Mobile App Development</a></li> -->
+                </ul>
+              </div>
+            </div>
+            <div class="col-lg-2 col-sm-6">
+              <div class="cs-footer_item">
+                
+                <h2 class="cs-widget_title" style="color:#cccc00;">Digital Marketing</h2>
+                <ul class="cs-menu_widget cs-mp0">
+                  <li>
+                    <a href="#">DSF</a>
+                  </li>
+                  <li>
+                    <a href="#">DMSS</a>
+                  </li>
+                  <li>
+                    <a href="#">Social Media Campaign</a>
+                  </li>
+                  
+                </ul>
+                <br>
+
+                <h2 class="cs-widget_title" style="color:#cccc00;">Internship Program</h2>
+                <ul class="cs-menu_widget cs-mp0">
+                  <li>
+                    <a href="#">DeSchooling</a>
+                  </li>
+                   <li>
+                    <a href="#">DeD Center</a>
+                  </li>
+                 <!-- <li>
+                    <a href="#">Social Media Campaign</a>
+                  </li> -->
+                  
+                </ul>
+                <br><br>
+                <h2 class="cs-widget_title" style="color:#cccc00;">Internet Marketing</h2>
+                <ul class="cs-menu_widget cs-mp0">
+                  <li><a href="#">Pay Per Click</a></li>
+                  <li><a href="#">Whatsapp Bulk Message</a></li>
+                  <li><a href="#">Payment Integration</a></li>
+                  <li><a href="#">Email Marketing</a></li>
+                  <li><a href="#">Social Media Marketing</a></li>
+                  <li><a href="#">B2B Listening</a></li>
+                  <!-- <li><a href="#">Mobile App Development</a></li> -->
+                </ul>
+              
+              </div>
+            </div>
+
+            <div class="col-lg-2 col-sm-6">
+              <div class="cs-footer_item">
+                <h2 class="cs-widget_title" style="color:#cccc00;">Organization</h2>
+                <ul class="cs-menu_widget cs-mp0">
+                  <li>
+                    <a href="#">WUs</a>
+                  </li>
+                  <li>
+                    <a href="#">Techno Crats</a>
+                  </li>
+                  <li>
+                    <a href="#">Nine 3/4 Quarters</a>
+                  </li>
+                  <li>
+                    <a href="#">Langaa</a>
+                  </li>
+                  <li>
+                    <a href="#">Community</a>
+                  </li>
+                  <li>
+                    <a href="#">DSF</a>
+                  </li>
+                </ul>
+                <br><br><br>
+                
+                 <h2 class="cs-widget_title" style="color:#cccc00;">Contact Us</h2>
+                <ul class="cs-menu_widget cs-mp0">
+                  <li><a href="#">Plus IT Business Park Thrissur, Kerala</a></li>
+                  <li><a href="#">info@plusitpark.com</a></li>
+                  <li><a href="#">+91 **********</a></li>
+                  <li><a href="#">www.plusitpark.com</a></li>
+                  <!-- <li><a href="#">Interactive Web Designing</a></li> -->
+                  <!-- <li><a href="#">Backend Solution</a></li> -->
+                  <!-- <li><a href="#">Mobile App Development</a></li> -->
+                </ul>
+              </div>
+            </div>
+            
+            
+            
+            <!-- <div class="col-lg-3 col-sm-6">
+              <div class="cs-footer_item">
+                <h2 class="cs-widget_title"></h2>
+                <div class="cs-newsletter cs-style1">
+                  <form action="#" class="cs-newsletter_form">
+                    <input type="email" class="cs-newsletter_input" placeholder="example@gmail.com">
+                    <button class="cs-newsletter_btn">
+                      <span>Send</span>
+                    </button>
+                  </form>
+                  <div class="cs-newsletter_text"> At vero eos et accusamus et iusto odio as part dignissimos ducimus qui blandit. </div>
+                </div>
+              </div>
+            </div> -->
+          </div>
+        </div>
+        
+      </div>
+      
+      <div class="container">
+        <div class="cs-bottom_footer">
+          <div class="cs-bottom_footer_left">
+            <div class="cs-copyright">Copyright © 2023 <a href=""> Plus IT Business Park</a></div>
+          </div>
+          <div class="cs-bottom_footer_right">
+            <ul class="cs-footer_links cs-mp0">
+              <li>
+                <a href="">FAQ</a>
+              </li>
+              <li>
+                <a href="">Privacy Policy</a>
+              </li>
+              <li>
+                <a href="">Terms of Use</a>
+              </li>
+              <li>
+                <a href="">Privacy Policy</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </footer>
---
+++ plus_it_park/Includes/header.php
@@ -0,0 +1,196 @@
+<header class="cs-site_header cs-style1 text-uppercase cs-sticky_header">
+      
+
+      <div class="cs-main_header">
+        <div class="container">
+          <div class="cs-main_header_in">
+            <div class="cs-main_header_left">
+              <a class="cs-site_branding" href="index.html">
+                <img src="assets/img/logoo.png" alt="Logo" >
+              </a>
+            </div>
+            <div class="cs-main_header_center">
+              <div class="cs-nav cs-primary_font cs-medium">
+                <ul class="cs-nav_list">
+                  <!-- <li class= " cs-mega_menu"><a href="index.html"><img src="assets/img/logoo.png" alt="Logo" style="height: auto; width: 130px;"></a></li> -->
+
+                  <li class= " cs-mega_menu"><a href="index.php">Home</a></li>
+                  
+                  <li class="menu-item-has-children">
+                    <a href="about.php">About Us</a>
+                    <ul>
+                      <li><a href="team.php">Our Clan</a></li>
+                      <li><a href="openings.php">Openings</a></li>
+                      <li><a href="internship.php">Careers</a></li>
+                      <li><a href="downloads.php">Downloads</a></li>
+                      
+                    </ul>
+                  </li>
+                  <li class="menu-item-has-children">
+                    <a href="services.php">What We Do</a>
+                    <ul>
+                      <li><a href="web_development.php">Web Development</a></li>
+                      <li><a href="#">Web 3 Blockchain Development</a></li>
+                      <li><a href="#">Mobile App Development</a></li>
+                      <li><a href="digital_marketing.php">Digital Marketing</a></li>
+                      <!-- <li><a href="#">Digital Sales Force</a></li> -->
+                      <li><a href="#">Industrial 4.O</a></li>
+                      <li><a href="internship.php">Training Program</a></li>
+                      <!-- <li><a href="#">Study Tours</a></li> -->
+                      <!-- <li><a href="#">D-Schooling</a></li>
+                      <li><a href="#">Community</a></li> -->
+                      <!-- <li><a href="#">Custome Made Software</a></li> -->
+                      
+
+                    </ul>
+                  </li>
+                  <li class="menu-item-has-children">
+                    <a href="#">Organization</a>
+                    <ul>
+                      <li><a href="#">Type 07 Technocrats</a></li>
+                      <li><a href="wus.php">Web Updates - Wus</a></li>
+                      <li><a href="#">Digital Sales Force - DSF</a></li>
+                      <li><a href="#">De Schooling</a></li>
+                      <li><a href="#">Langaa</a></li>
+                      <li><a href="#">9 3/4 Quarters</a></li>
+                      <li><a href="#">Sathoshi Gangz - Community</a></li>
+                    </ul>
+                  </li>
+                  <li class="menu-item-has-children">
+                    <a href="life at plus.php">Life at Plus IT Park</a>
+                    <ul>
+                      <li><a href="#">Events & Cultural Programs</a></li>
+                      <li><a href="#">Blog</a></li>
+                      <li><a href="#">Testimonials</a></li>
+                    </ul>
+                  </li>
+                  <div class="cs-main_header_right">
+                    <div class="cs-toolbox">
+                      <button class="btn" style="color:white;"><a href="contact.php">Lets Talk</a></button>
+                    </div>
+                  </div>
+                </ul>
+              </div>
+            </div>
+            <div class="cs-main_header_right">
+              <div class="cs-toolbox">
+                <span class="cs-icon_btn">
+                  <span class="cs-icon_btn_in">
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                  </span>
+                </span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </header>
+    <div class="cs-side_header">
+      <button class="cs-close"></button>
+      <div class="cs-side_header_overlay"></div>
+      <div class="cs-side_header_in" style="width:1200px;">
+        <div class="cs-side_header_shape"></div>
+        <a class="cs-site_branding" href="index.html">
+          <img src="assets/img/logoo.png" alt="Logo">
+        </a>
+        <div class="cs-side_header_box">
+          <h2 class="cs-side_header_heading"> The complete end to end IT Partner for Businesses, Global brands, IT Products and Services to help companies worldwide in Web Development, Design, SEO and Services.</h2>
+        </div>
+        <div class="cs-side_header_box">
+          <!-- <h3 class="cs-side_header_title cs-primary_color">Contact Us</h3> -->
+           <div class="row">
+            <div class="col-lg-3 col-sm-6">
+              <div class="cs-footer_item">
+                <h2 class="cs-widget_title" style="color:#cccc00;">Organizations</h2>
+                <ul class="cs-menu_widget cs-mp0">
+                  <li>
+                    <a href="#">WUs</a>
+                  </li>
+                  <li>
+                    <a href="#">Techno Crats</a>
+                  </li>
+                  <li>
+                    <a href="#">Nine 3/4 Quarters</a>
+                  </li>
+                  <li>
+                    <a href="#">Langaa</a>
+                  </li>
+                  <li>
+                    <a href="#">Community</a>
+                  </li>
+                  <li>
+                    <a href="#">DSF</a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+            <div class="col-lg-3 col-sm-6">
+              <div class="cs-footer_item">
+                <h2 class="cs-widget_title" style="color:#cccc00;">Quicklinks</h2>
+                <ul class="cs-menu_widget cs-mp0">
+                  <li>
+                    <a href="#">Home</a>
+                  </li>
+                  <li>
+                    <a href="#">What We Do</a>
+                  </li>
+                  <li>
+                    <a href="#">About Us</a>
+                  </li>
+                  <li>
+                    <a href="#">Organizations</a>
+                  </li>
+                  <li>
+                    <a href="#">Life at Plus IT Park</a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+            <div class="col-lg-3 col-sm-6">
+              <div class="cs-footer_item">
+                <h2 class="cs-widget_title" style="color:#cccc00;">4 Ds</h2>
+                <ul class="cs-menu_widget cs-mp0">
+                  <li>
+                    <a href="#">Design</a>
+                  </li>
+                  <li>
+                    <a href="#">Development</a>
+                  </li>
+                  <li>
+                    <a href="#">Digital marketing</a>
+                  </li>
+                  <li>
+                    <a href="#">Deschooling</a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+            
+            <div class="col-lg-3 col-sm-6">
+              <div class="cs-footer_item">
+                <h2 class="cs-widget_title" style="color:#cccc00;">Contact Us</h2>
+                <ul class="cs-menu_widget cs-mp0">
+                  <li>
+                    <a href="#">Plus IT Business Park Thrissur, Kerala</a>
+                  </li>
+                  <li>
+                    <a href="#">info@plusitpark.com</a>
+                  </li>
+                  <li>
+                    <a href="#">+91 **********</a>
+                  </li>
+                  <li>
+                    <a href="#">www.plusitpark.com</a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          
+        </div>
+       
+      </div>
+    </div>
