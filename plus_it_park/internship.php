c7ea3d268ceb8f3693d20ffc58689ee9f3bc9dbe
AljoAntony24 <aljoantony24@gmail.com>
2023-11-16 13:38:38 +0530

    new files

--- plus_it_park/internship.php
+++ plus_it_park/internship.php
@@ -166,7 +166,7 @@
               </div> -->
             <!-- </a> -->
              <div class="cs-height_40 cs-height_lg_40"></div>
-            <a href="web3.php" class="cs-text_btn">
+            <a href="internship" class="cs-text_btn">
               <span>More About Web 3 Development</span>
               <svg width="26" height="12" viewBox="0 0 26 12" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path d="M25.5303 6.53033C25.8232 6.23744 25.8232 5.76256 25.5303 5.46967L20.7574 0.696699C20.4645 0.403806 19.9896 0.403806 19.6967 0.696699C19.4038 0.989593 19.4038 1.46447 19.6967 1.75736L23.9393 6L19.6967 10.2426C19.4038 10.5355 19.4038 11.0104 19.6967 11.3033C19.9896 11.5962 20.4645 11.5962 20.7574 11.3033L25.5303 6.53033ZM0 6.75H25V5.25H0V6.75Z" fill="currentColor"></path>
@@ -187,7 +187,7 @@
               </div> -->
             <!-- </a> -->
              <div class="cs-height_40 cs-height_lg_40"></div>
-            <a href="web_development.php" class="cs-text_btn">
+            <a href="../internship/web_development.html" class="cs-text_btn">
               <span>More About Web Development</span>
               <svg width="26" height="12" viewBox="0 0 26 12" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path d="M25.5303 6.53033C25.8232 6.23744 25.8232 5.76256 25.5303 5.46967L20.7574 0.696699C20.4645 0.403806 19.9896 0.403806 19.6967 0.696699C19.4038 0.989593 19.4038 1.46447 19.6967 1.75736L23.9393 6L19.6967 10.2426C19.4038 10.5355 19.4038 11.0104 19.6967 11.3033C19.9896 11.5962 20.4645 11.5962 20.7574 11.3033L25.5303 6.53033ZM0 6.75H25V5.25H0V6.75Z" fill="currentColor"></path>
@@ -210,7 +210,7 @@
               </div> -->
             <!-- </a> -->
              <div class="cs-height_90 cs-height_lg_40"></div>
-            <a href="flutter_development.php" class="cs-text_btn">
+            <a href="../internship/flutter_development.html" class="cs-text_btn">
               <span>More About Flutter Development</span>
               <svg width="26" height="12" viewBox="0 0 26 12" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path d="M25.5303 6.53033C25.8232 6.23744 25.8232 5.76256 25.5303 5.46967L20.7574 0.696699C20.4645 0.403806 19.9896 0.403806 19.6967 0.696699C19.4038 0.989593 19.4038 1.46447 19.6967 1.75736L23.9393 6L19.6967 10.2426C19.4038 10.5355 19.4038 11.0104 19.6967 11.3033C19.9896 11.5962 20.4645 11.5962 20.7574 11.3033L25.5303 6.53033ZM0 6.75H25V5.25H0V6.75Z" fill="currentColor"></path>
@@ -235,7 +235,7 @@
             <!-- </a> -->
             <div class="cs-height_80 cs-height_lg_40"></div>
             <div class="cs-height_90 cs-height_lg_40"></div>
-            <a href="digital_marketing.php" class="cs-text_btn">
+            <a href="../internship/digital_marketing.html" class="cs-text_btn">
               <span>More About Digital Marketing</span>
               <svg width="26" height="12" viewBox="0 0 26 12" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path d="M25.5303 6.53033C25.8232 6.23744 25.8232 5.76256 25.5303 5.46967L20.7574 0.696699C20.4645 0.403806 19.9896 0.403806 19.6967 0.696699C19.4038 0.989593 19.4038 1.46447 19.6967 1.75736L23.9393 6L19.6967 10.2426C19.4038 10.5355 19.4038 11.0104 19.6967 11.3033C19.9896 11.5962 20.4645 11.5962 20.7574 11.3033L25.5303 6.53033ZM0 6.75H25V5.25H0V6.75Z" fill="currentColor"></path>
