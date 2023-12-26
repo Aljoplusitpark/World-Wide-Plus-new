638f07a3dba9e639f2f029c3afdd9a9f905c57a3
AljoAntony24 <aljoantony24@gmail.com>
2023-12-26 18:31:32 +0530

    new file

---
+++ plus_it_park/refund_policy.php
@@ -0,0 +1,129 @@
+<!DOCTYPE html>
+<html class="no-js" lang="en">
+
+<head>
+  <!-- Meta Tags -->
+  <meta charset="utf-8">
+  <meta http-equiv="x-ua-compatible" content="ie=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="author" content="Laralink">
+  <!-- Favicon Icon -->
+  <link rel="icon" href="assets/img/favicon.png">
+  <!-- Site Title -->
+  <title>Privacy Policy</title>
+  <link rel="stylesheet" href="assets/css/plugins/bootstrap.min.css">
+  <link rel="stylesheet" href="assets/css/plugins/fontawesome.min.css">
+  <link rel="stylesheet" href="assets/css/plugins/slick.css">
+  <link rel="stylesheet" href="assets/css/plugins/lightgallery.min.css">
+  <link rel="stylesheet" href="assets/css/plugins/animate.css">
+  <link rel="stylesheet" href="assets/css/style.css">
+</head>
+
+<body>
+  <div class="cs-preloader cs-center">
+    <div class="cs-preloader_in"></div>
+  </div>
+  <!-- Start Header Section -->
+  <?php include("includes/header.php") ?>
+  
+  
+  <!-- Start Hero -->
+  <!-- <div class="cs-page_heading cs-style1 cs-center text-center cs-bg" data-src="assets/img/sbg7.png">
+    <div class="container">
+      <div class="cs-page_heading_in">
+        <h1 class="cs-page_title cs-font_50 cs-white_color">Our Team</h1>
+        <ol class="breadcrumb text-uppercase">
+          <li class="breadcrumb-item"><a href="index.html">Home</a></li>
+          <li class="breadcrumb-item active">Team</li>
+        </ol>
+      </div>
+    </div>
+  </div> -->
+  <!-- End Hero -->
+  <!-- End Why Choose -->
+  <div class="cs-height_145 cs-height_lg_80"></div>
+  <!-- Start Team Section -->
+  <section>
+    <div class="container">
+      <div class="cs-section_heading cs-style1 text-center">
+        <!-- <h3 class="cs-section_subtitle">Our Team</h3> -->
+        <h2 class="cs-section_title">Refund Policy</h2>
+      </div>
+      <div class="cs-height_90 cs-height_lg_45"></div>
+    </div>
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-12 col-sm-6">
+          <p style="text-align: justify;">Web Development, Web 3.0 Development, Digital Marketing, Mobile Application Development, Custom-made System Software, and Web Products At Plus IT Business Park, we strive to provide exceptional services and products to our clients. We understand that situations may arise where a refund is necessary. Please read the following guidelines regarding our refund policy</p> 
+          <h4 class="cs-section_title">Refund Eligibility</h4>
+          <ul>
+            <li style="text-align: justify;">Services: Refunds are applicable within 30 days from the date of purchase for services that have not been initiated or started.</li>
+            <li style="text-align: justify;">Products: Refunds for products (software, web products) are subject to the terms specified on the product page or upon delivery.</li>
+            <li style="text-align: justify;">Non-Refundable Items:Custom Development Work: Any work that has been initiated or completed on a custom project is non-refundable.</li>
+            <li style="text-align: justify;">Digital Marketing Services: Services rendered or time spent on digital marketing campaigns are non-refundable.</li>
+
+          </ul>
+          <h4 class="cs-section_title">Refund Process</h4>
+
+          <p style="text-align: justify;">To request a refund, clients must contact our support team via email at support@plusitbusinesspark.com within the eligible refund period. Please provide the relevant order details, reason for the refund request, and any supporting documentation or information.</p>
+          <h4 class="cs-section_title">Refund Considerations</h4>
+          <p style="text-align: justify;">Refunds will be processed within 15 business days upon approval of the refund request. The refund amount may be subject to deductions for any work completed or expenses incurred up to the point of the refund request.</p>
+          <h4 class="cs-section_title">Exceptions</h4>
+          <p style="text-align:justify;">In exceptional circumstances or situations beyond our control where services or products cannot be delivered as promised, we reserve the right to provide a full or partial refund at our discretion.</p>
+          <h4 class="cs-section_title">Contact Information</h4>
+          <p style="text-align:justify;">For any inquiries or assistance regarding refunds, please contact our support team at support@plusitbusinesspark.com.</p>
+          <h4 class="cs-section_title">Policy Updates</h4>
+          <p style="text-align:justify;">Plus IT Business Park reserves the right to amend, modify, or update this refund policy at any time without prior notice. The revised policy will be effective upon posting on our website.</p>
+          <br>
+          <p>This refund policy is specifically for services and products offered by Plus IT Business Park concerning web development, digital marketing, mobile app development, custom system software, and web products.</p>
+          <p>All refund requests will be assessed and processed in accordance with this policy.</p>
+            
+          <div class="cs-height_40 cs-height_lg_30"></div>
+        </div> 
+
+       
+    </div>
+  </section>
+  <!-- End Team Section -->
+  <!-- <div class="cs-height_70 cs-height_lg_50"></div> -->
+  <!-- Start CTA -->
+  
+  <!-- End CTA -->
+  <!-- Start Footer -->
+  <?php include("includes/footer.php") ?>
+  <!-- End Footer -->
+
+  <span class="cs-scrollup">
+    <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path d="M0 10L1.7625 11.7625L8.75 4.7875V20H11.25V4.7875L18.225 11.775L20 10L10 0L0 10Z" fill="currentColor"/>
+    </svg>      
+  </span>
+  
+  <!-- Start Video Popup -->
+  <div class="cs-video_popup">
+    <div class="cs-video_popup_overlay"></div>
+    <div class="cs-video_popup_content">
+      <div class="cs-video_popup_layer"></div>
+      <div class="cs-video_popup_container">
+        <div class="cs-video_popup_align">
+          <div class="embed-responsive embed-responsive-16by9">
+            <iframe class="embed-responsive-item" src="about:blank"></iframe>
+          </div>
+        </div>
+        <div class="cs-video_popup_close"></div>
+      </div>
+    </div>
+  </div>
+  <!-- End Video Popup -->
+
+  <!-- Script -->
+  <script src="assets/js/plugins/jquery-3.6.0.min.js"></script>
+  <script src="assets/js/plugins/isotope.pkg.min.js"></script>
+  <script src="assets/js/plugins/jquery.slick.min.js"></script>
+  <script src="assets/js/plugins/jquery.counter.min.js"></script>
+  <script src="assets/js/plugins/lightgallery.min.js"></script>
+  <script src="assets/js/plugins/wow.min.js"></script>
+  <script src="assets/js/plugins/gsap.min.js"></script>
+  <script src="assets/js/main.js"></script>
+</body>
+</html>
