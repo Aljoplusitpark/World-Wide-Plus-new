34461906ef5f6316f885e2b33fcfbe4838c7e6ca
AljoAntony24 <aljoantony24@gmail.com>
2024-01-03 10:16:53 +0530

    new

---
+++ plus_it_park/Services.php
@@ -0,0 +1,205 @@
+<!DOCTYPE html>
+<html class="no-js" lang="en">
+
+<head>
+  <!-- Meta Tags -->
+  <meta charset="utf-8">
+  <meta http-equiv="x-ua-compatible" content="ie=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="author" content="Laralink">
+  <!-- Favicon Icon -->
+  <link rel="icon" href="assets/img/favicon.png">
+  <!-- Site Title -->
+  <title>What We Do</title>
+  <link rel="stylesheet" href="assets/css/plugins/bootstrap.min.css">
+  <link rel="stylesheet" href="assets/css/plugins/fontawesome.min.css">
+  <link rel="stylesheet" href="assets/css/plugins/slick.css">
+  <link rel="stylesheet" href="assets/css/plugins/lightgallery.min.css">
+  <link rel="stylesheet" href="assets/css/plugins/animate.css">
+  <link rel="stylesheet" href="assets/css/style.css">
+</head>
+
+<body>
+  <div class="cs-preloader cs-center">
+    <div class="cs-preloader_in"></div>
+  </div>
+  <!-- Start Header Section -->
+  <?php include("includes/header.php") ?>
+  
+  <!-- End Header Section -->
+  
+  <!-- Start Hero -->
+  <div class="cs-page_heading cs-style1 cs-center text-center cs-bg" data-src="assets/img/services.jpeg">
+    <div class="container">
+      <div class="cs-page_heading_in">
+        <h1 class="cs-page_title cs-font_50 cs-white_color"></h1>
+        <!-- <ol class="breadcrumb text-uppercase">
+          <li class="breadcrumb-item"><a href="index.html"></a></li>
+          <li class="breadcrumb-item active"></li>
+        </ol> -->
+      </div>
+    </div>
+  </div>
+  <!-- End Hero -->
+  <div class="cs-height_150 cs-height_lg_80"></div>
+  <!-- Start Portfolio -->
+  <div class="container">
+    <div class="cs-portfolio_1_heading">
+      <div class="cs-section_heading cs-style1">
+        
+        <h2 class="cs-section_title">Our Services</h2>
+        <h3 class="cs-section_subtitle">Empowering Innovation, Transforming Solutions: Your Trusted Partner in Software Excellence.</h3>
+      </div>
+      <div class="cs-isotop_filter cs-style1">
+        <ul class="cs-mp0 cs-center">
+          <li class="active"><a href="#" data-filter="*">All</a></li>
+          <li><a href="#" data-filter=".web_design">Web Design</a></li>
+          <li><a href="#" data-filter=".ui_ux_design">UI/UX Design</a></li>
+          <li><a href="#" data-filter=".mobile_apps">Mobile Apps</a></li>
+          <li><a href="#" data-filter=".logo_design">Logo Design</a></li>
+        </ul>
+      </div>
+    </div>
+    <div class="cs-height_90 cs-height_lg_15"></div>
+    <div class="cs-isotop cs-style1 cs-isotop_col_3 cs-has_gutter_24">
+      <div class="cs-grid_sizer"></div>
+      <div class="cs-isotop_item ui_ux_design">
+        <a href="portfolio-details.html" class="cs-portfolio cs-style1 cs-type1">
+          <div class="cs-portfolio_hover"></div>
+          <div class="cs-portfolio_bg cs-bg" data-src="assets/img/web.jpeg"></div>
+          <div class="cs-portfolio_info">
+            <div class="cs-portfolio_info_bg cs-accent_bg"></div>
+            <!-- <h2 class="cs-portfolio_title">WEB</h2> -->
+            <div class="cs-portfolio_subtitle">WebContent</div>
+          </div>
+        </a>
+      </div><!-- .cs-isotop_item -->
+      <div class="cs-isotop_item logo_design">
+        <a href="portfolio-details.html" class="cs-portfolio cs-style1 cs-type1">
+          <div class="cs-portfolio_hover"></div>
+          <div class="cs-portfolio_bg cs-bg" data-src="assets/img/ui.jpeg"></div>
+          <div class="cs-portfolio_info">
+            <div class="cs-portfolio_info_bg cs-accent_bg"></div>
+            <!-- <h2 class="cs-portfolio_title">Colorful Art Work</h2> -->
+            <div class="cs-portfolio_subtitle">UI/UX...</div>
+          </div>
+        </a>
+      </div><!-- .cs-isotop_item -->
+      <div class="cs-isotop_item web_design">
+        <a href="portfolio-details.html" class="cs-portfolio cs-style1 cs-type1">
+          <div class="cs-portfolio_hover"></div>
+          <div class="cs-portfolio_bg cs-bg" data-src="assets/img/app.jpeg"></div>
+          <div class="cs-portfolio_info">
+            <div class="cs-portfolio_info_bg cs-accent_bg"></div>
+            <!-- <h2 class="cs-portfolio_title">Colorful Art Work</h2> -->
+            <div class="cs-portfolio_subtitle">App Development.....</div>
+          </div>
+        </a>
+      </div><!-- .cs-isotop_item -->
+      <div class="cs-isotop_item cs-w66 mobile_apps">
+        <a href="portfolio-details.html" class="cs-portfolio cs-style1 cs-type1">
+          <div class="cs-portfolio_hover"></div>
+          <div class="cs-portfolio_bg cs-bg" data-src="assets/img/dig.jpeg"></div>
+          <div class="cs-portfolio_info">
+            <div class="cs-portfolio_info_bg cs-accent_bg"></div>
+            <!-- <h2 class="cs-portfolio_title">Colorful Art Work</h2> -->
+            <div class="cs-portfolio_subtitle">Digital Marketing....</div>
+          </div>
+        </a>
+      </div><!-- .cs-isotop_item -->
+      <div class="cs-isotop_item ui_ux_design">
+        <a href="portfolio-details.html" class="cs-portfolio cs-style1 cs-type1">
+          <div class="cs-portfolio_hover"></div>
+          <div class="cs-portfolio_bg cs-bg" data-src="assets/img/dsf.jpeg"></div>
+          <div class="cs-portfolio_info">
+            <div class="cs-portfolio_info_bg cs-accent_bg"></div>
+            <!-- <h2 class="cs-portfolio_title">Colorful Art Work</h2> -->
+            <div class="cs-portfolio_subtitle">DSF...</div>
+          </div>
+        </a>
+      </div><!-- .cs-isotop_item -->
+      <div class="cs-isotop_item web_design">
+        <a href="portfolio-details.html" class="cs-portfolio cs-style1 cs-type1">
+          <div class="cs-portfolio_hover"></div>
+          <div class="cs-portfolio_bg cs-bg" data-src="assets/img/des.jpeg"></div>
+          <div class="cs-portfolio_info">
+            <div class="cs-portfolio_info_bg cs-accent_bg"></div>
+            <!-- <h2 class="cs-portfolio_title">Colorful Art Work</h2> -->
+            <div class="cs-portfolio_subtitle">Deschooling...</div>
+          </div>
+        </a>
+      </div><!-- .cs-isotop_item -->
+      <div class="cs-isotop_item cs-w66 logo_design">
+        <a href="portfolio-details.html" class="cs-portfolio cs-style1 cs-type1">
+          <div class="cs-portfolio_hover"></div>
+          <div class="cs-portfolio_bg cs-bg" data-src="assets/img/web3.jpeg"></div>
+          <div class="cs-portfolio_info">
+            <div class="cs-portfolio_info_bg cs-accent_bg"></div>
+            <!-- <h2 class="cs-portfolio_title">Colorful Art Work</h2> -->
+            <div class="cs-portfolio_subtitle">Web3...</div>
+          </div>
+        </a>
+      </div><!-- .cs-isotop_item -->
+    </div>
+    <div class="cs-height_90 cs-height_lg_40"></div>
+    <div class="text-center">
+      <a href="#" class="cs-text_btn">
+        <span>Load More</span>
+        <svg width="26" height="12" viewBox="0 0 26 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path d="M25.5303 6.53033C25.8232 6.23744 25.8232 5.76256 25.5303 5.46967L20.7574 0.696699C20.4645 0.403806 19.9896 0.403806 19.6967 0.696699C19.4038 0.989593 19.4038 1.46447 19.6967 1.75736L23.9393 6L19.6967 10.2426C19.4038 10.5355 19.4038 11.0104 19.6967 11.3033C19.9896 11.5962 20.4645 11.5962 20.7574 11.3033L25.5303 6.53033ZM0 6.75H25V5.25H0V6.75Z" fill="currentColor"></path>
+        </svg>                
+      </a>
+    </div>
+  </div>
+  <!-- End Portfolio -->
+  <div class="cs-height_145 cs-height_lg_80"></div>
+  <!-- Start CTA -->
+  <section>
+    <!-- <div class="cs-cta cs-style1 cs-bg text-center cs-shape_wrap_1 cs-position_1 rounded-0" data-src="assets/img/cta_bg_2.jpeg" >
+      <div class="cs-shape_1"></div>
+      <div class="cs-shape_1"></div>
+      <div class="cs-shape_1"></div>
+      <div class="cs-cta_in">
+        <h2 class="cs-cta_title cs-semi_bold cs-m0">info@plusitpark.com</h2>
+      </div>
+    </div> -->
+  </section>
+  <!-- End CTA -->
+  <!-- Start Footer -->
+  <?php include("includes/footer.php") ?>
+  <!-- End Footer -->
+
+  <span class="cs-scrollup">
+    <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path d="M0 10L1.7625 11.7625L8.75 4.7875V20H11.25V4.7875L18.225 11.775L20 10L10 0L0 10Z" fill="currentColor"/>
+    </svg>      
+  </span>
+  
+  <!-- Start Video Popup -->
+  <div class="cs-video_popup">
+    <div class="cs-video_popup_overlay"></div>
+    <div class="cs-video_popup_content">
+      <div class="cs-video_popup_layer"></div>
+      <div class="cs-video_popup_container">
+        <div class="cs-video_popup_align">
+          <div class="embed-responsive embed-responsive-16by9">
+            <iframe class="embed-responsive-item" src="about:blank"></iframe>
+          </div>
+        </div>
+        <div class="cs-video_popup_close"></div>
+      </div>
+    </div>
+  </div>
+  <!-- End Video Popup -->
+
+  <!-- Script -->
+  <script src="assets/js/plugins/jquery-3.6.0.min.js"></script>
+  <script src="assets/js/plugins/isotope.pkg.min.js"></script>
+  <script src="assets/js/plugins/jquery.slick.min.js"></script>
+  <script src="assets/js/plugins/jquery.counter.min.js"></script>
+  <script src="assets/js/plugins/lightgallery.min.js"></script>
+  <script src="assets/js/plugins/wow.min.js"></script>
+  <script src="assets/js/plugins/gsap.min.js"></script>
+  <script src="assets/js/main.js"></script>
+</body>
+</html>
